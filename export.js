bdc917d82e7d2aae97bf7278bbefb667feb052a5
Priyansh Bhardwaj <priyanshbhj@gmail.com>
2019-11-05 03:17:40 +0530

    Stoppage Time Field Added

--- export.js
+++ export.js
@@ -27,39 +27,39 @@ module.exports = (mailer, actualStart, actualEnd) => {
         main.cell(2,3).string('18:00:00').style(bold);
 
         machines.forEach(machine => {
-			
+
 			todayRecord.push({
 				name: machine.name,
 				date: actualStart.toDateString(),
 				stopDurations: machine.stopDurations
 			});
-			
+
             let sheet=workbook.addWorksheet(machine.name);
             sheet.cell(1,1).string('Name').style(bold);
             sheet.cell(1,2).string(machine.name).style(style);
-            sheet.cell(3,1).string('From').style(bold);
-            sheet.cell(3,2).string('To').style(bold);
-            sheet.cell(3,3).string('Duration').style(bold);
-            
-			var totalOperation=actualEnd-actualStart;
+            sheet.cell(4,1).string('From').style(bold);
+            sheet.cell(4,2).string('To').style(bold);
+            sheet.cell(4,3).string('Duration').style(bold);
+
+			var totalOperation=actualEnd-actualStart,totalStoppage=0;
             for(let i=0;i<machine.stopDurations.length;i++){
                 let from=new Date(machine.stopDurations[i].from),to=new Date(machine.stopDurations[i].to);
                 let duration=Math.floor((to-from)/1000);
-				totalOperation=totalOperation-(to-from);
+				        totalStoppage+=(to-from);
                 let hh=Math.floor(duration/3600),mm=Math.floor((duration%3600)/60),ss=(duration%60);
                 if(hh<10) hh='0'+hh;
                 if(mm<10) mm='0'+mm;
                 if(ss<10) ss='0'+ss;
 
-                sheet.cell(i+4,1).string(from.toTimeString().slice(0,8)).style(style);
-                sheet.cell(i+4,2).string(to.toTimeString().slice(0,8)).style(style);
-                sheet.cell(i+4,3).string(hh+':'+mm+':'+ss).style(style);
+                sheet.cell(i+5,1).string(from.toTimeString().slice(0,8)).style(style);
+                sheet.cell(i+5,2).string(to.toTimeString().slice(0,8)).style(style);
+                sheet.cell(i+5,3).string(hh+':'+mm+':'+ss).style(style);
             }
 
             if(!machine.functioning){
                 let from=new Date(machine.stopTime),to=new Date();
                 let duration=Math.floor((to-from)/1000);
-				totalOperation = totalOperation-(to-from);
+				        totalStoppage += (to-from);
                 let hh=Math.floor(duration/3600),mm=Math.floor((duration%3600)/60),ss=(duration%60);
                 if(hh<10) hh='0'+hh;
                 if(mm<10) mm='0'+mm;
@@ -69,15 +69,26 @@ module.exports = (mailer, actualStart, actualEnd) => {
                 sheet.cell(machine.stopDurations.length+4,2).string(to.toTimeString().slice(0,8)).style(style);
                 sheet.cell(machine.stopDurations.length+4,3).string(hh+':'+mm+':'+ss).style(style);
             }
-			
-			if(totalOperation<0) totalOperation=0;
-			totalOperation=Math.floor(totalOperation/1000);
-			let hours=Math.floor(totalOperation/3600), minutes=Math.floor((totalOperation%3600)/60), seconds=(totalOperation%60);
-			if(hours<10) hours='0'+hours;
-			if(minutes<10) minutes='0'+minutes;
-			if(seconds<10) seconds='0'+seconds;
-			sheet.cell(2,1).string('Operating Time').style(bold);
-			sheet.cell(2,2).string(hours+':'+minutes+':'+seconds).style(style);
+
+          totalOperation-=totalStoppage;
+    			if(totalOperation<0) totalOperation=0;
+    			totalOperation=Math.floor(totalOperation/1000);
+    			let hours=Math.floor(totalOperation/3600), minutes=Math.floor((totalOperation%3600)/60), seconds=(totalOperation%60);
+    			if(hours<10) hours='0'+hours;
+    			if(minutes<10) minutes='0'+minutes;
+    			if(seconds<10) seconds='0'+seconds;
+    			sheet.cell(2,1).string('Operating Time').style(bold);
+    			sheet.cell(2,2).string(hours+':'+minutes+':'+seconds).style(style);
+
+          totalStoppage=Math.floor(totalStoppage/1000);
+    			hours=Math.floor(totalStoppage/3600);
+          minutes=Math.floor((totalStoppage%3600)/60);
+          seconds=(totalStoppage%60);
+    			if(hours<10) hours='0'+hours;
+    			if(minutes<10) minutes='0'+minutes;
+    			if(seconds<10) seconds='0'+seconds;
+    			sheet.cell(3,1).string('Stoppage Time').style(bold);
+    			sheet.cell(3,2).string(hours+':'+minutes+':'+seconds).style(style);
         });
 		Record.insertMany(todayRecord);
         workbook.writeToBuffer()
@@ -87,7 +98,7 @@ module.exports = (mailer, actualStart, actualEnd) => {
 				to: 'atulmist@gmail.com',
 				subject: 'Stop Timings of '+new Date().toDateString(),
 				text: 'Please find excel sheet in attachment',
-				attachments: [{'filename': 'stopTimings.xlsx', 'content': sheet}]
+				attachments: [{'filename': 'StopTimings '+new Date().toDateString()+'.xlsx', 'content': sheet}]
 			},(err,info) => {
 				if(err){
 					return console.log(err);
